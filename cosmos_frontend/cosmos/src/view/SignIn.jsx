dcf7afd8148146c13d7fbd910e0d956089bd104a
Dslim1996 <ging35@naver.com>
2022-01-21 19:02:03 +0900

    화면 css 수정

--- cosmos_frontend/cosmos/src/view/SignIn.jsx
+++ cosmos_frontend/cosmos/src/view/SignIn.jsx
@@ -41,14 +41,6 @@ function Signin() {
             console.log('아니야')
         }
     }
-
-    const gettest = () => {
-        Authapi.test().then(res => {
-            console.log(res.data);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
     
     return (
         <div>
@@ -69,14 +61,21 @@ function Signin() {
                 <div className="btn_area">
                     <div className='btn full' onClick={login}>로그인</div>
                 </div>
-                <div>
+                <div className='find_id_pw'>
                     <span>아이디 찾기</span>
                     <span>비밀번호 찾기</span>
                 </div>
             </div>
-            <div className="bottom_content">
-                <div className="btn" onClick={gettest}>
-                    gettest
+            <div className='bottom_content'>
+                <div className='bottom_title'>SNS 계정을 연동해 간편하게 로그인해보세요</div>
+                <div className="btn_area">
+                    <div className='btn reversal'>카카오톡</div>
+                    <div className='btn reversal'>네이버</div>
+                    <div className='btn reversal'>구글</div>
+                    <div className='btn reversal'>페이스북</div>
+                </div>
+                <div className='bottom_info'>
+                    COS 온라인 스토어는 더현대닷컴이 운영·제공하는 사이트로 기존 회원은 더현대닷컴 아이디와 비밀번호로 바로 이용 가능합니다.
                 </div>
             </div>
         </div>   
