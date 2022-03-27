03bf58f5ef79846b213775944ee52857acbdad34
Dslim1996 <ging35@naver.com>
2022-03-27 14:37:56 +0900

    @GetMapping PathVariable 수정

--- cosmos_backend/cosmos/src/main/java/com/shopping/cosmos/controller/ProductController.java
+++ cosmos_backend/cosmos/src/main/java/com/shopping/cosmos/controller/ProductController.java
@@ -7,17 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 	
 	//상품리스트 보내면서 컬러는 중복제거해서 List로 전송
-	@GetMapping("/${gender}/${category}")
+	@GetMapping("/{gender}/{category}")
 	String productList() {
 		return null;
 	}
 	
-	@GetMapping("/product/${id}")
+	@GetMapping("/product/{id}")
 	String productDetail() {
 		return null;
 	}
 	
-	@GetMapping("/cart/${userID}")
+	@GetMapping("/cart/{userID}")
 	String cartPage() {
 		return null;
 	}
