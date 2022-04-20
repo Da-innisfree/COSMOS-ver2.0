c9038ae779357013b92cb81108effbed2f7338cc
Da-innisfree <dainni.k@gmail.com>
2022-04-20 22:17:29 +0900

    product 로직 수정

--- cosmos_backend/cosmos/src/main/java/com/shopping/cosmos/controller/ProductController.java
+++ cosmos_backend/cosmos/src/main/java/com/shopping/cosmos/controller/ProductController.java
@@ -1,26 +1,43 @@
 package com.shopping.cosmos.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shopping.cosmos.vo.CategoryVO;
+import com.shopping.cosmos.vo.ProductDetailVO;
+import com.shopping.cosmos.vo.ProductVO;
+
 
 @RestController
 public class ProductController {
 	
 	//상품리스트 보내면서 컬러는 중복제거해서 List로 전송
 	@GetMapping("/{gender}/{category}")
-	String productList() {
+	public List<ProductVO> productList() {
 		return null;
 	}
 	
 	@GetMapping("/product/{id}")
-	String productDetail() {
+	public List<ProductVO> productDetail() {
 		return null;
 	}
 	
 	@GetMapping("/cart/{userID}")
-	String cartPage() {
+	public String cartPage() {
 		return null;
 	}
+	
+	@GetMapping("/{gender}/{category}/detail")
+	public List<String> categoryDetail(){
+		return null;
+	}
+	
+	
+	
+	
 	
 	
 	
